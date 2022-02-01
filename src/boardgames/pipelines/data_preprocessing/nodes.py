d3523866cd3cfc13838e76b56a549d6581f55726
Ian Anderson <anderson.ian2011@gmail.com>
2022-02-01 18:20:57 -0500

    adds eda and new pipeline features for details, removes jupyter cells

--- src/boardgames/pipelines/data_preprocessing/nodes.py
+++ src/boardgames/pipelines/data_preprocessing/nodes.py
@@ -1,7 +1,20 @@
 import pandas as pd 
 
 def preprocess_ratings(ratings: pd.DataFrame) -> pd.DataFrame:
-    
+    """Removes two url columns
+
+    Args:
+        ratings (pd.DataFrame): Straight csv downloaded from internet
+
+    Returns:
+        pd.DataFrame: Defaults in the csv downloaded from internet, less two columns
+    """
     cols_to_drop = ['url','thumbnail']
     ratings.drop(cols_to_drop, axis=1, inplace=True)
     return ratings
+
+def preprocess_details(details: pd.DataFrame) -> pd.DataFrame:
+    cols_to_keep = [each for each in details.columns.tolist() if 'boardgame' in each]
+
+    details = details.loc[:,cols_to_keep]
+    return details
--- src/boardgames/pipelines/data_preprocessing/pipeline.py
+++ src/boardgames/pipelines/data_preprocessing/pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import preprocess_ratings
+from .nodes import preprocess_ratings, preprocess_details
 
 def create_pipeline(**kwargs):
     return Pipeline(
@@ -11,5 +11,12 @@ def create_pipeline(**kwargs):
                 outputs="prepro_ratings",
                 name="preprocess_ratings_node",
             ),
+            node(
+                func=preprocess_details,
+                inputs="details",
+                outputs="prepro_details",
+                name="preprocess_details_node",
+            ),
+
         ]
     )
