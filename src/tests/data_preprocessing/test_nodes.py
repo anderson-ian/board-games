2045926e81006e34cab8614508fabc7614189187
Ian Anderson <anderson.ian2011@gmail.com>
2022-02-14 19:27:12 -0500

    adds test for other column removal

--- src/tests/data_preprocessing/test_nodes.py
+++ src/tests/data_preprocessing/test_nodes.py
@@ -2,7 +2,8 @@ import pytest
 import pandas as pd
 
 from src.boardgames.pipelines.data_preprocessing.nodes import (
-    preprocess_ratings
+    preprocess_ratings,
+    preprocess_details
 )
 
 class TestPrimaryNodes:
@@ -27,4 +28,30 @@ class TestPrimaryNodes:
         
         actual_df = preprocess_ratings(test_df)
         assert actual_df.equals(expected_df)
-        
+
+    def test_preprocess_details(self):
+        """
+        Confirm that column names are dropped.
+        """
+        test_df = pd.DataFrame({
+                                'boardgame' : [],
+                                'description' : [],
+                                'col_A' : [],
+                                'col_B' : []
+                                 })
+
+        expected_df = pd.DataFrame({
+                                'col_A' : [],
+                                'col_B' : []
+                                 })
+
+        actual_df = preprocess_details(test_df)
+
+        if 'boardgame' not in actual_df.columns.to_list() and \
+           'description' not in actual_df.columns.to_list():
+            output = True
+        else:
+            output = False
+
+        assert output
+        assert actual_df.equals(expected_df)
